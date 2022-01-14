b89fe1ebde029c4c75d62730c6c74c27a30e6aaf
Aenoris <aenoriss@gmail.com>
2022-01-14 03:40:41 -0300

    zxzcz

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Box from "./Utils/Box";
 import ReactDOM from 'react-dom';
 import CameraControls from "./Utils/Camera";
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, Suspense } from 'react'
 import Grid from "./Utils/Board/Grid";
 import { Canvas, useFrame, useLoader, extend } from '@react-three/fiber'
 import SidePanel from "./Utils/SidePanel/SidePanel";
 import BasicTile from "./ModelComponent/BasicTile";
 import './App.css';
+import { Environment } from '@react-three/drei'
 
 //Turns OrbitControls into a JSX element.
 // extend({ OrbitControls });
@@ -17,22 +18,30 @@ function App() {
   const handleData = (data) => {
     setselectedTile(data);
   }
-  
+
+  console.log(Environment)
+
   return (
     <div className="App">
-      <SidePanel data={selectedTile}/>
+      <SidePanel data={selectedTile} />
       <Canvas>
-        <color attach="background" args={["#229aca"]} />
-        <CameraControls/>
-        {/* <Box position={[-1.5, 0, 0]} /> */}
-        {/* <Box position={[0, 0, 0]} /> */}
-        {/* <Box position={[1.5, 0, 0]} /> */}
-        <Grid type={0} data={handleData}/>
-        <Grid type={[1]}/>
-        {/* <BasicTile/> */}
-        {/* <axesHelper/> */}
-        <pointLight position={[10, 10, 10]} />
-        <ambientLight />
+        <Suspense fallback={"loading"}>
+          <color attach="background" args={["#229aca"]} />
+          <CameraControls />
+          <Grid type={0} data={handleData} />
+          <Grid type={[1]} />
+          {/* <Environment
+            background={true}
+            files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']}
+            path={"/Environment"}
+            preset={null}
+          /> */}
+          {/* <BasicTile/> */}
+          {/* <axesHelper/> */}
+          <pointLight position={[10, 10, 10]} />
+          <ambientLight />
+        </Suspense>
+
       </Canvas>
     </div>
   );
--- src/Utils/SidePanel/SidePanel.js
+++ src/Utils/SidePanel/SidePanel.js
@@ -1,18 +1,112 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { DoubleSide } from "three";
 import './SidePanel.css';
 
 
 function SidePanel(props) {
-    console.log("HEHEH", props.data)
+    const [landPopulate, setLandPopulate] = useState([
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+        {
+            type: "plain",
+            level: 1
+        },
+    ]);
+
+    useEffect(() => {
+        if (window.ethereum.isMetaMask) {
+
+            // useDispatch(setMetamaskLoading(true));
+
+            window.ethereum.request({ method: 'eth_requestAccounts' }).then(async () => {
+                console.log("Connected")
+
+                // Change state 
+                // metamaskConnect -> true
+
+                // const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+                // const signer = provider.getSigner();
+
+                // const blockNumber = await provider.getBlockNumber();
+
+                // console.log("Block Number:", blockNumber)
+
+                // const mainInstance = new ethers.Contract(data.contract_address, mainContract.abi, provider);
+
+
+                // const mainWithSigner = mainInstance.connect(signer);
+
+                // const accounts = await window.ethereum.request({ method: 'eth_accounts' })
+
+
+                // if (accounts.length > 0) {
+
+                //   console.log("breaks?: ", accounts)
+
+                //   let isRegistered = await mainWithSigner.userToRegistered(accounts[0]).catch(e => {
+                //     console.log("error: ", e)
+                //   });
+
+                //   console.log("isRegistered: ", isRegistered);
+                // }
+
+            }).catch(() => {
+                console.log("Not Connected");
+            })
+        }
+    }, [])
+
     return (
         <div>
             {props.data && <div id="menuPanel">
-                <h1>Add Chunk at: </h1>
-                <h2>{"x: "+ Math.ceil( props.data[0]) + "  y: " + Math.ceil(props.data[2]) + "  z: "+ Math.ceil(props.data[1])}</h2>
+                <h1>Tile </h1>
+                <h2>{"x: " + Math.ceil(props.data[0]) + "  y: " + Math.ceil(props.data[2]) + "  z: " + Math.ceil(props.data[1])}</h2>
                 <h1>Inventory</h1>
-                <button class ="button">Add</button>
+                <div className="tileList">
+                    {landPopulate.map((land) => {
+                        return (
+                            <div className="tile">
+                                <img></img>
+                                <h3>{land.type}</h3>
+                                <h3>{land.level}</h3>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className="footer">
+                    <button class="button">Create Land</button>
+                    <button class="button">Add Land</button>
+                </div>
+
+
             </div>}
         </div>
     )
